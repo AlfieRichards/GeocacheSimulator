2793083042a900a5bc3a44cd2ae89bb2dac72629
Alfie <87566534+Frosty11234@users.noreply.github.com>
2022-10-05 14:22:17 +0100

    Added folders

--- Assets/Scripts/Chest/CrateManager.cs
+++ Assets/Scripts/Chest/CrateManager.cs
@@ -29,11 +29,29 @@ public class CrateManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(chests.Count == 0){
+        if(chests.Count == 0)
+        {
             Debug.Log("No chests found in save system");
-
+            GenerateChests();
         }
         
+    }
+
+    void GenerateChests()
+    {
+        int cratesAmnt = Random.Range(0,12);
+        for(int i = 0; i < cratesAmnt; i++)
+        {
+            //generates chest location
+            float randomX = Random.Range(topRightCoordinate.x, BottomLeftCoordinate.x);
+            float randomY = Random.Range(topRightCoordinate.y, BottomLeftCoordinate.y);
+
+            //instantiates chest
+            Vector2 finalRandomLoc = new Vector2(randomX, randomY);
+            float[] crateLocation = {randomX, randomY};
+            chests.Add(crateLocation);
+            Instantiate(cratePrefab, finalRandomLoc, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
